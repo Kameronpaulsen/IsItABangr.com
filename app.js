20137b8d4d5e52bb43f4f0f260eb26c63d84ec9d
kameronpaulsen <kameronpaulsen@gmail.com>
2017-11-13 12:20:41 -0700

    id search

--- app.js
+++ app.js
@@ -75,27 +75,17 @@ request.post(authOptions, function(error, response, body) {
 app.get('/search', function(req,res){
 	var responseText;
 	var options = {
-		url: 'https://api.spotify.com/v1/06HL4z0CvFAxyc27GXpf02',
+		url: 'https://api.spotify.com/v1/artists/06HL4z0CvFAxyc27GXpf02',
 		headers: {
 			'Authorization': 'Bearer ' + token
 		},
 		json: true
-	};
-	request.post(authOptions, function(error, response, body) {
-		if (!error && response.statusCode === 200) {
-			var options = {
-				url: 'https://api.spotify.com/v1/artists/06HL4z0CvFAxyc27GXpf02',
-				headers: {
-					'Authorization': 'Bearer ' + token
-				},
-				json: true
-			};
-			request.get(options, function(error, result, body) {
-				console.log(JSON.stringify(body));
-				responseText = JSON.stringify(body);
-			});
-		}
-	});
+
+		request.get(options, function(error, result, body) {
+			console.log(JSON.stringify(body));
+			responseText = JSON.stringify(body);
+		});
+	}
 	res.send(responseText);
 });
 
