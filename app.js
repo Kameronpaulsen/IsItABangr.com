7256237c2c677a7c7d8ca8310d08188522634817
kameronpaulsen <kameronpaulsen@gmail.com>
2017-11-13 13:15:22 -0700

    id search

--- app.js
+++ app.js
@@ -94,6 +94,10 @@ app.get('/search/:keyword?', function(req,res){
 		},
 		json: true
 	}
+	request.get(options, function(error, result, body) {
+		id = body.artists.items[1].id;
+		console.log(id);
+	});
 	res.send(result);
 });
 
